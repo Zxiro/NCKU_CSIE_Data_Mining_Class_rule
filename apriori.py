77aba08b407a34bbd2cb36ec0dd23b36a4e67a68
Zxiro <a0963212510@gmail.com>
2020-10-17 22:39:24 +0800

    Init commit

---
+++ apriori.py
@@ -0,0 +1,44 @@
+import csv
+import math
+import pandas as pd
+
+def clean_data(csv): #clean the data generate by IBM #data_10^4trans
+    df = pd.read_csv('./'+csv+'.csv')
+    print(df)
+    print(df.index)
+    print(df.shape)
+    df['customer'] = ""
+    df['transaction'] = ""
+    df['item'] = ""
+    df.rename(columns={"         1          1         42":"tmp"}, inplace = True)
+    df["tmp"][0] = "         1          1         42"
+
+    for i in range(df.shape[0]):
+        tmp = df["tmp"][i].split()
+        df['customer'][i] = tmp[0]
+        df['transaction'][i] = tmp[1]
+        df['item'][i] = tmp[2]
+        print(i)
+
+    df.drop(["tmp"], inplace = True, axis = 1)
+    print(df)
+    df.to_csv('./clean_data.csv')
+
+if __name__ == "__main__":
+    df = pd.read_csv('./clean_data.csv')
+    df.drop(['customer'], axis = 1, inplace = True)
+    items = df.groupby("item")
+    print(items.groups) #0-962 kinds of items appear in which transaction
+    item_dict = items.groups
+    trans_list = [] #size = 962, each ele is a list contain the transaction that item appear
+    for key in item_dict:
+        trans_list.append(set(item_dict[key]))
+    min_conf = 0.75
+    min_sup = 0.5
+    print(len(trans_list[0]))
+    print(trans_list[0])
+    single = 0
+    for i in range(len(trans_list)):
+        single+= len(trans_list[i])
+    print(single)
+    #Right now there exists a list of set which represents the transaction number that  item which number is the number of ele appears
