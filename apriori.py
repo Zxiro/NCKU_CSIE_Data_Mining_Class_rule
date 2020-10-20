70a83e5943c3b26da79a9ba25e7d5fd671bbaa3d
Zxiro <a0963212510@gmail.com>
2020-10-20 23:04:41 +0800

    Finish apriori algo

--- apriori.py
+++ apriori.py
@@ -1,3 +1,4 @@
+import re
 import csv
 import math
 import time
@@ -14,39 +15,68 @@ def clean_data(csv): #clean the data generate by IBM #data_10^4trans
 
     for i in range(df.shape[0]):
         tmp = df["tmp"][i].split()
-        df['customer'][i] = tmp[0]
         df['transaction'][i] = tmp[1]
         df['item'][i] = tmp[2]
 
     df.drop(["tmp"], inplace = True, axis = 1)
     df.to_csv('./clean_data.csv')
-def get_set_min_sup(set_, dict):
-    num = 0
-    set_num = 0
-    for key in item_dict:
-        item_list = []
-        for i in list(item_dict[key]): # The item exist in the transaction
-            item = df['item'][i]
-            item_list.append(item) # The list of item of #key transaction
-        item_set = set(item_list)
-        if( len(set_ & item_set ) >= len(set_)):
-            num+=1
-        for l in range(len(item_list)+1):
-            if l > 0:
-                for ele in combinations(item_list, l):
-                    #print(ele)
-                    set_num += 1
-        print(key, set_num)
-    print(num)
-    print(set_num) #set_num = 400832
 
+def get_set(set_, dict, trans_num):
+    l = 1
+    set_dict = {}
+    for set__ in set_:
+        for ele in combinations(set__, l):
+            if len(ele) !=0:
+                ele_ = set((ele))
+                if (str(ele_ ) in set_dict):
+                    set_dict[str(ele_ )]  = (set_dict[str(ele_ )][0], set_dict[str(ele_ )][1]+1)
+                else:
+                    set_dict[str(ele_ )] = (ele_, 1)
+            else: break
+    l+=1
+    set_dict = check_fit_min_sup(set_dict, 0.01, trans_num)
+    while l<4:
+        tmp_set = set()
+        for key in set_dict:
+            tmp_set.add((set_dict[key][0].pop()))
+        set_dict = {}
+        for ele_ in combinations(tmp_set, l): #個數為一的freq_set裡能夠進行L個分組           
+            ele = set(list(ele_))
+            print(ele)
+            if len(ele) !=0:    
+                for i in range(len(set_)):
+                    if((ele & set_[i]) == (ele)):
+                        if (str(ele) in set_dict):
+                            set_dict[str(ele)]  = (set_dict[str(ele)][0], set_dict[str(ele)][1]+1)
+                        else:
+                            set_dict[str(ele)] = (ele, 1)
+            else: break   
+        print(len(set_dict))
+        set_dict = check_fit_min_sup(set_dict, 0.01, trans_num)
+        print(len(set_dict))
+        print("Into next L")
+        time.sleep(5)
+        l +=1
+    
+def check_fit_min_sup(dict_, min_sup, trans_num):
+    for key in list(dict_):
+        if(dict_[key][1]/trans_num <min_sup):
+            dict_.pop(key)
+    return dict_
 
     
 if __name__ == "__main__":
     clean_data('data_ntrans_1_tlen_5_nitems_1')
     df = pd.read_csv('./clean_data.csv')
-    df.drop(['customer'], axis = 1, inplace = True)
-    items = df.groupby("transaction")
-    #print(items.groups) #0-962 kinds of items appear in which transaction
+    items = df.groupby("transaction")  #print(items.groups) #0-962 kinds of items appear in which transaction
     item_dict = items.groups
-    get_set_min_sup({420}, item_dict)
+    item_set = []
+    for key in item_dict:
+        tmp = set()
+        for i in list(item_dict[key]): # The item exist in the transaction
+            item = df['item'][i]
+            tmp.add(item) # The list of item of #key transaction
+        item_set.append(tmp)
+    get_set(item_set, item_dict, len(item_set))#print(item_set)# The list of set of each transaction
+    #print(len(item_set))# The number of all transaction
+    
